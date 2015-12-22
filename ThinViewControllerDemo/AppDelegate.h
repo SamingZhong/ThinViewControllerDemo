62725b889b8fd885111fbbcd806f62ab6bde666a
钟晓明 <zhongxiaoming@easytoo.net>
2015-12-22 23:19:54 +0800

    Initial Commit

---
+++ ThinViewControllerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThinViewControllerDemo
+//
+//  Created by SamingZhong on 15/12/22.
+//  Copyright © 2015年 zhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
